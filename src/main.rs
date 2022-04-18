31ccbf49ac1eea1b767365dddf463b4bc2ff9d5b
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2022-04-18 23:06:50 +0100

    Add clap + missing poem dep and config struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+use clap::Parser;
+use tracing_subscriber::filter::LevelFilter;
+
 pub(crate) mod models;
 pub(crate) mod search;
+
+
+#[derive(Debug, Parser)]
+#[clap(version, about)]
+pub struct Config {
+    #[clap(short, long, env, default_value = "127.0.0.1:7700")]
+    /// The address for the webserver to bind to.
+    bind: String,
+
+    #[clap(long, env, default_value = "info")]
+    /// The level in which to display logs.
+    log_level: LevelFilter,
+
+    #[clap(long, env, default_value = "127.0.0.1:9042")]
+    /// A list of known cluster nodes seperated by a `;`.
+    cluster_nodes: String,
+}
+
 
 #[tokio::main]
 async fn main() {
